39971270f08a8da35187ea3c36cd7575165a1d13
R4f4y04 <pinchspy.pk@gmail.com>
2025-01-18 03:52:59 +0500

    perfected onboarding screen made logo as well

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -19,7 +19,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       title: 'Welcome to Bachat: Budget',
       description:
           'Your personal expense tracker designed for hassle-free budget management.',
-      image: 'assets/welcome.png',
+      image: 'assets/welcome.gif',
     ),
     OnboardingPage(
       title: 'Changing Theme',
